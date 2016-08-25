752e3c944603978af4362fc356f86b6625a6664d
Thomas Weinert <thomas@weinert.info>
2016-08-25 13:18:21 +0200

    Add translations tabled names as constants, define database access classes

--- src/Content/Glossary.php
+++ src/Content/Glossary.php
@@ -4,4 +4,6 @@ class GlossaryContentGlossary extends PapayaDatabaseRecord  {
   protected $_fields = [
     'id' => 'glossary_id'
   ];
+
+  protected $_tableName = GlossaryContentTables::TABLE_GLOSSARIES;
 }
---
+++ src/Content/Tables.php
@@ -0,0 +1,12 @@
+<?php
+
+interface GlossaryContentTables {
+
+  const TABLE_GLOSSARIES = 'glossary';
+  const TABLE_GLOSSARY_TRANSLATIONS = 'glossary_trans';
+
+  const TABLE_TERMS = 'glossaryentries';
+  const TABLE_GLOSSARY_TERMS = 'glossaryentries_translations';
+
+  const TABLE_IGNORE_WORDS = 'glossary_ignorewords';
+}
