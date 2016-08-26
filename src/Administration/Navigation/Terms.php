6df13f8fc03332e1ccb48f4d84340bc74665ca78
Thomas Weinert <thomas@weinert.info>
2016-08-26 19:20:46 +0200

    Terms list

--- src/Administration/Navigation/Terms.php
+++ src/Administration/Navigation/Terms.php
@@ -7,8 +7,23 @@ class GlossaryAdministrationNavigationTerms extends PapayaUiControlCommand {
    */
   private $_filterDialog = NULL;
 
+  /**
+   * @var PapayaUiListview
+   */
+  private $_listview = NULL;
+  /**
+   * @var GlossaryContentTerms
+   */
+  private $_terms = NULL;
+  /**
+   * @var PapayaUiToolbarPaging
+   */
+  private $_paging;
+
   public function appendTo(PapayaXmlElement $parent) {
     $parent->append($this->filterDialog());
+    $this->listview()->toolbars->topLeft->elements[] = $this->paging();
+    $parent->append($this->listview());
   }
 
   public function filterDialog(PapayaUiDialog $filterDialog = NULL) {
@@ -28,4 +43,77 @@ class GlossaryAdministrationNavigationTerms extends PapayaUiControlCommand {
     return $this->_filterDialog;
   }
 
+  /**
+   * @param PapayaUiListview $listview
+   * @return PapayaUiListview
+   */
+  public function listview(PapayaUiListview $listview = NULL) {
+    if (isset($listview)) {
+      $this->_listview = $listview;
+    } elseif (NULL === $this->_listview) {
+      $this->_listview = $listview = new PapayaUiListview();
+      $listview->papaya($this->papaya());
+      $listview->caption = new PapayaUiStringTranslated('Terms');
+      $listview->builder(
+        $builder = new PapayaUiListviewItemsBuilder($this->terms())
+      );
+      $listview->builder()->callbacks()->onCreateItem = array($this, 'callbackCreateItem');
+      $listview->builder()->callbacks()->onCreateItem->context = $builder;
+      $listview->parameterGroup($this->parameterGroup());
+      $listview->parameters($this->parameters());
+    }
+    return $this->_listview;
+  }
+
+  public function paging(PapayaUiToolbarPaging $paging = NULL) {
+    if (isset($paging)) {
+      $this->_paging = $paging;
+    } elseif (NULL === $this->_paging) {
+      $this->_paging = $paging = new PapayaUiToolbarPaging(
+        [$this->parameterGroup(), 'page'],
+        $this->terms()->absCount()
+      );
+      $paging->papaya($this->papaya());
+    }
+    return $this->_paging;
+  }
+
+  public function terms(GlossaryContentTerms $terms = NULL) {
+    if (isset($terms)) {
+      $this->_terms = $terms;
+    } elseif (NULL == $this->_terms) {
+      $this->_terms = new GlossaryContentTerms();
+      $this->_terms->papaya($this->papaya());
+      $this->_terms->activateLazyLoad(
+        [
+          'language_id' => $this->papaya()->administrationLanguage->id
+        ],
+        $this->paging()->itemsPerPage,
+        $this->paging()->currentOffset
+      );
+    }
+    return $this->_terms;
+  }
+
+  /**
+   * @param PapayaUiListviewItemsBuilder $builder
+   * @param PapayaUiListviewItems $items
+   * @param mixed $element
+   * @param mixed $index
+   */
+  public function callbackCreateItem($builder, $items, $element, $index) {
+    $items[] = $item = new PapayaUiListviewItem('items-page', $element['term']);
+    $item->papaya($this->papaya());
+    $item->reference->setParameters(
+      array(
+        'mode' => 'terms',
+        'cmd' => 'change',
+        'term_id' => $element['id']
+      ),
+      $this->parameterGroup()
+    );
+    $item->selected = (
+      $this->parameters()->get('term_id') == $element['id']
+    );
+  }
 }
--- src/Content/Tables.php
+++ src/Content/Tables.php
@@ -6,7 +6,7 @@ interface GlossaryContentTables {
   const TABLE_GLOSSARY_TRANSLATIONS = 'glossary_trans';
 
   const TABLE_TERMS = 'glossaryentries';
-  const TABLE_GLOSSARY_TERMS = 'glossaryentries_translations';
+  const TABLE_TERM_TRANSLATIONS = 'glossaryentries_trans';
 
   const TABLE_IGNORE_WORDS = 'glossary_ignorewords';
 }
