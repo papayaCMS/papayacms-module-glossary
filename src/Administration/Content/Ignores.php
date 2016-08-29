6c79ccd324c0aa046f51a505bf86bfafa5a0a6c0
Thomas Weinert <thomas@weinert.info>
2016-08-29 13:38:43 +0200

    Edit glossary ignore words

--- src/Administration/Content/Ignores.php
+++ src/Administration/Content/Ignores.php
@@ -1,8 +1,53 @@
 <?php
 
-class GlossaryAdministrationContentIgnores extends PapayaUiControlCommand {
+class GlossaryAdministrationContentIgnores extends PapayaUiControlCommandDialogDatabaseRecord  {
 
-  public function appendTo(PapayaXmlElement $parent) {
+  private $_action = self::ACTION_SAVE;
 
+  public function __construct($action) {
+    parent::__construct(new GlossaryContentIgnore(), $this->_action = $action);
+  }
+
+  public function createDialog() {
+    $dialog = parent::createDialog();
+    $loaded = $this->record()->load(
+      [
+        'id' => $this->parameters()->get('ignore_word_id', 0),
+        'language_id' => $this->parameters()->get('language_id', $this->papaya()->administrationLanguage->id)
+      ]
+    );
+    $dialog->parameterGroup($this->parameterGroup());
+    $dialog->hiddenFields()->merge(
+      [
+        'mode' => 'ignore-words',
+        'cmd' => $this->_action == self::ACTION_DELETE ? 'delete' : 'change',
+        'ignore_word_id' => $this->parameters()->get('ignore_word_id', 0),
+        'language_id' => $this->parameters()->get('language_id', $this->papaya()->administrationLanguage->id)
+      ]
+    );
+    switch ($this->_action) {
+    case self::ACTION_DELETE :
+      if (!$loaded) {
+        return $dialog;
+      }
+      $dialog->caption = new PapayaUiStringTranslated('Delete');
+      $dialog->fields[] = new PapayaUiDialogFieldInformation(
+        new PapayaUiStringTranslated('Delete ignore word: "%s"', [$this->record()['word']]),
+        'places-trash'
+      );
+      $dialog->buttons[] = new PapayaUiDialogButtonSubmit(new PapayaUiStringTranslated('Delete'));
+      break;
+    default :
+      $dialog->caption = new PapayaUiStringTranslated($loaded ? 'Edit' : 'Add');
+      $dialog->data()->merge($this->record());
+      $dialog->fields[] = $field = new PapayaUiDialogFieldInput(
+        new PapayaUiStringTranslated('Word'),
+        'word'
+      );
+      $field->setMandatory(TRUE);
+      $dialog->buttons[] = new PapayaUiDialogButtonSubmit(new PapayaUiStringTranslated('Save'));
+      break;
+    }
+    return $dialog;
   }
 }
